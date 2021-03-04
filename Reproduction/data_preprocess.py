ba8e9e82848a535bd0005ab0bbe710e005a4d4d8
Eben Hu <EbenHu@outlook.com>
2021-03-04 09:34:04 +0800

    Update data_preprocess.py

--- Reproduction/data_preprocess.py
+++ Reproduction/data_preprocess.py
@@ -17,14 +17,14 @@ def unit_conversion(df):
 
 
 if __name__ == '__main__':
-    US101_part1 = pd.read_csv('./Datasets/US101/vehicle-trajectory-data/0750am-0805am/trajectories-0750am-0805am.csv')
+    US101_part1 = pd.read_csv('./trajectories-0750am-0805am.csv')
     df1 = unit_conversion(US101_part1)
     # print(df1.head(4))
     # print(df1.columns)
     # print(df1.index)
     # print(len(df1))
     vehicle_id = df1['Vehicle_ID']
-    vehicle_id_list = []
+    vehicle_id_list = [] # all vehicles during this period
     for i in range(len(df1)):
         if vehicle_id[i] not in vehicle_id_list:
             vehicle_id_list.append(vehicle_id[i])
@@ -33,43 +33,52 @@ if __name__ == '__main__':
     # filter the changing-lane vehicles
     f1 = df1[['Vehicle_ID', 'Lane_ID']]
     # print(f1.head(4))
+    change_id = []
+    change_row = []
     change_right_id = []
     change_right_row = []
     change_left_id = []
     change_left_row = []
-    lane_keep_id = vehicle_id_list
     for i in range(len(f1)):
         if i == 0:
             continue
         else:
-            if f1['Lane_ID'][i] == 2 or f1['Lane_ID'][i] == 3 or f1['Lane_ID'][i] == 4:
-                if f1['Vehicle_ID'][i] == f1['Vehicle_ID'][i - 1] and f1['Lane_ID'][i] == f1['Lane_ID'][i - 1] + 1:
-                    if f1['Lane_ID'][i] not in change_right_id:
-                        change_right_id.append(f1['Vehicle_ID'][i])
-                    change_right_row.append(i-1)
-                    change_right_row.append(i)
-                if f1['Vehicle_ID'][i] == f1['Vehicle_ID'][i - 1] and f1['Lane_ID'][i] == f1['Lane_ID'][i - 1] - 1:
-                    if f1['Lane_ID'][i] not in change_left_id:
-                        change_left_id.append(f1['Vehicle_ID'][i])
-                    change_left_row.append(i-1)
-                    change_left_row.append(i)
-            else:
-                continue
+            # select the vehicles in lane 2, lane 3 and lane 4 first
+            # if f1['Lane_ID'][i] == 2 or f1['Lane_ID'][i] == 3 or f1['Lane_ID'][i] == 4:
+            if f1['Vehicle_ID'][i] == f1['Vehicle_ID'][i - 1] and f1['Lane_ID'][i] != f1['Lane_ID'][i - 1]:
+                if f1['Lane_ID'][i] not in change_id:
+                    change_id.append(f1['Lane_ID'][i])
+                change_row.append(i-1)
+                change_row.append(i)
+            # changing right
+            if f1['Vehicle_ID'][i] == f1['Vehicle_ID'][i - 1] and f1['Lane_ID'][i] == f1['Lane_ID'][i - 1] + 1:
+                if f1['Lane_ID'][i] not in change_right_id:
+                    change_right_id.append(f1['Vehicle_ID'][i])
+                change_right_row.append(i-1)
+                change_right_row.append(i)
+            # changing left
+            if f1['Vehicle_ID'][i] == f1['Vehicle_ID'][i - 1] and f1['Lane_ID'][i] == f1['Lane_ID'][i - 1] - 1:
+                if f1['Lane_ID'][i] not in change_left_id:
+                    change_left_id.append(f1['Vehicle_ID'][i])
+                change_left_row.append(i-1)
+                change_left_row.append(i)
+            # else:
+            #     continue
 
     # filter the keeping-straight vehicles
-    print(len(lane_keep_id))
+    lane_keep_id1 = vehicle_id_list
+    lane_keep_id2 = []
     for i in range(len(change_left_id)):
-        if change_left_id[i] in lane_keep_id:
-            lane_keep_id.remove(change_left_id[i])
-    print(len(lane_keep_id))
+        if change_left_id[i] in lane_keep_id1:
+            lane_keep_id1.remove(change_left_id[i])
     for i in range(len(change_right_id)):
-        if change_right_id[i] in lane_keep_id:
-            lane_keep_id.remove(change_right_id[i])
-    print(len(lane_keep_id))
+        if change_right_id[i] in lane_keep_id1:
+            lane_keep_id1.remove(change_right_id[i])
+    print(f'The number of keeping straight vehicles is {len(lane_keep_id1)} (with method 1)')
 
-    # print(len(change_right_id))
-    # print(change_right_id)
-    # print(len(change_left_id))
-    # print(change_left_id)
-    # print(len(lane_keep_id))
-    # print(change_right_row)
+    for i in vehicle_id_list:
+        if i not in change_id:
+            lane_keep_id2.append(i)
+    print(f'The number of keeping straight vehicles is {len(lane_keep_id2)} (with method 2)')
+    print(f'The number of turning right vehicles is {len(change_right_id)}')
+    print(f'The number of turning light vehicles is {len(change_left_id)}')
